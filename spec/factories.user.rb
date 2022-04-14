0fba4277db3afb141033189ff73041bfcb837ada
root <jacksterdragon@gmail.com>
2022-04-14 05:04:22 +0000

    The tests still do not work because I have not been able to figure out how to login with devise during a test

---
+++ spec/factories.user.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory(:user) do
+    email { "jane.doe@hey.com" }
+    password { "Password" }
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'devise'
+
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
@@ -8,6 +10,11 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Warden::Test::Helpers
+end
 
 require 'simplecov'
 SimpleCov.start 'rails' do
